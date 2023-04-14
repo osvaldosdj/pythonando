0ca0d489816d715fdc44b5a55979324e44e5e07a
osvaldosdj <osvaldosdj@gmail>
2023-04-14 17:40:50 -0300

    Continuação dia 01

---
+++ type_event/eventos/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Evento
+
+
+# Register your models here.
+
+admin.site.register(Evento)
--- type_event/usuarios/views.py
+++ type_event/usuarios/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.urls import reverse
+from django.contrib import auth
 
 # Create your views here.
 
@@ -15,17 +17,47 @@ def cadastro(request):
             senha = request.POST.get('senha')
             confirmar_senha = request.POST.get('confirmar_senha')
 
+            if len(senha) < 5:
+                messages.add_message(request, constants.ERROR, 'Senha não preenche requisito de segurança')
+                return redirect (reverse('cadastro'))
+
+            if username == "" or senha == "":
+                messages.add_message(request, constants.ERROR, 'Usuário ou senha não preenchido')
+                return redirect (reverse('cadastro'))
+
             if not senha == confirmar_senha:
               messages.add_message(request, constants.ERROR, 'Senhas Não conferem')
-              return redirect ('/usuarios/cadastro')
+              return redirect (reverse('cadastro'))
 
             user = User.objects.filter(username=username)
 
             if user.exists():
                 messages.add_message(request, constants.ERROR, 'Usuário ou senha incorretos')
-                return redirect('/usuarios/cadastro')
+                return redirect (reverse('cadastro'))
 
             user = User.objects.create_user(username=username, email=email, password=senha)
             messages.add_message(request, constants.SUCCESS, 'Usuário Cadastrado com Sucesso')
 
-            return HttpResponse('Teste')
+            return redirect (reverse('login'))
+
+        
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        senha = request.POST.get("senha")
+
+        if username == "" or senha == "":
+                messages.add_message(request, constants.ERROR, 'Usuário ou senha não preenchido')
+                return redirect (reverse('login'))
+
+        user = auth.authenticate(username=username, password=senha)
+
+        if not user:
+            messages.add_message(request, constants.ERROR, 'Usuário ou Senha inválidos cadastrado')
+            return redirect (reverse('login'))
+
+        auth.login(request, user)
+        return redirect ('/eventos/novo_evento/')
+
